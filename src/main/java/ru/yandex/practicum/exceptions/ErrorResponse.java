1edec62654d0498afa10ab796b1809db869f5937
Евгений <89537796626@mail.ru>
2023-05-11 17:22:30 +0700

    test

--- src/main/java/ru/yandex/practicum/exceptions/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/exceptions/ErrorResponse.java
@@ -6,7 +6,7 @@ public class ErrorResponse {
     //подробное описание
     private String description;
 
-    public ErrorResponse(String error, String description){
+    public ErrorResponse(String error, String description) {
         this.error = error;
         this.description = description;
     }
